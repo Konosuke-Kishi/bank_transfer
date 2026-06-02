6f1be390bbccb3d5bb8048c85e6718d6b6c6e00e
Konosuke-Kishi <konosuke.kishi@gmail.com>
2026-06-02 16:06:05 +0900

    import順序変更

--- transfer_shinsei.py
+++ transfer_shinsei.py
@@ -3,11 +3,11 @@
 # ======================================================
 from config import CONFIG
 from config_shinsei import CONFIG as SHINSEI_CONFIG
-from undetected_geckodriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from undetected_geckodriver import Firefox
 import subprocess, re, undetected_chromedriver as uc
 
 # ======================================================
